69c5320512c861a58cef28213ad76decd7c0bf9e
Sharma Priti <ps0130815@gmail.com>
2025-07-18 09:53:32 +0530

    Update os prac1.java

--- os prac1.java
+++ os prac1.java
@@ -1,54 +1,69 @@
 import java.util.Scanner;
 
-public class SimpleFCFS {
+public class FCFS{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input number of processes
         System.out.print("Enter number of processes: ");
         int n = sc.nextInt();
-        
-        int[] arrival = new int[n];
-        int[] burstTime = new int[n];
-        int[] waitingTime = new int[n];
-        int[] turnAroundTime = new int[n];
-        int[] completionTime = new int[n];
 
-        // Input arrival and burst times
+        int[] arrivalTime = new int[n];
+        int[] burstTime = new int[n];
+        int[] completionTime = new int[n];
+        int[] turnAroundTime = new int[n];
+        int[] waitingTime = new int[n];
+
+        
         for (int i = 0; i < n; i++) {
-            System.out.println("Enter Arrival Time and Burst Time for Process " + (i + 1) + ":");
-            arrival[i] = sc.nextInt();
+            System.out.print("Enter arrival time for process " + (i + 1) + ": ");
+            arrivalTime[i] = sc.nextInt();
+            System.out.print("Enter burst time for process " + (i + 1) + ": ");
             burstTime[i] = sc.nextInt();
         }
 
-        // Calculate waiting time
-        waitingTime[0] = 0;
-        for (int i = 1; i < n; i++) {
-            waitingTime[i] = waitingTime[i - 1] + burstTime[i - 1];
+        
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arrivalTime[j] < arrivalTime[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            
+            int temp = arrivalTime[i];
+            arrivalTime[i] = arrivalTime[minIndex];
+            arrivalTime[minIndex] = temp;
+          
+            temp = burstTime[i];
+            burstTime[i] = burstTime[minIndex];
+            burstTime[minIndex] = temp;
         }
 
-        // Calculate turnaround time
-        for (int i = 0; i < n; i++) {
-            turnAroundTime[i] = waitingTime[i] + burstTime[i];
-        }
-        //completion time
-         for (int i = 0; i < n; i++) {
-            completionTime[i] = waitingTime[i] +  burstTime[i];
-        }
-        
-        
+        int currentTime = 0;
 
-        // Display results
-        System.out.println("\nProcess\tBurst\tCompletion\tWaiting\tTurnaround");
-        int totalWT = 0, totalTAT = 0;
+       
         for (int i = 0; i < n; i++) {
-            System.out.println("P" + (i + 1) + "\t" + burstTime[i] + "\t" + completionTime[i] +"\t"+ "\t"     + waitingTime[i] + "\t"    + turnAroundTime[i]);
-            totalWT += waitingTime[i];
+            if (currentTime < arrivalTime[i]) {
+                currentTime = arrivalTime[i];  
+            }
+            currentTime += burstTime[i];
+            completionTime[i] = currentTime;
+            turnAroundTime[i] = completionTime[i] - arrivalTime[i];  // TAT = CT - AT
+            waitingTime[i] = turnAroundTime[i] - burstTime[i];      // WT = TAT - BT0
+        }
+
+        // Print results
+        System.out.println("\nProcess\tAT\tBT\tCT\tTAT\tWT");
+        int totalTAT = 0, totalWT = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.println((i + 1) + "\t" + arrivalTime[i] + "\t" + burstTime[i] + "\t" +
+                               completionTime[i] + "\t" + turnAroundTime[i] + "\t" + waitingTime[i]);
             totalTAT += turnAroundTime[i];
+            totalWT += waitingTime[i];
         }
 
-        System.out.println("\nAverage Waiting Time: " + (float) totalWT / n);
-        System.out.println("Average Turnaround Time: " + (float) totalTAT / n);
+        System.out.printf("\nAverage Turnaround Time: %.2f\n", (double) totalTAT / n);
+        System.out.printf("Average Waiting Time: %.2f\n", (double) totalWT / n);
 
         sc.close();
     }
